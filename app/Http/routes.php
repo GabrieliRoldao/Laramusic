b961d20f3c2d47d6da0e1470c0dd3b17fbd5f56a
Gabrieli Roldão <groldao@outlook.com>
2017-02-15 18:54:27 -0200

    Adicionado rotas para os métodos de adicionar e editar estilos

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -8,7 +8,25 @@ Route::group(['prefix' => 'painel', 'namespace' => 'Painel'], function ()
 {
     Route::get('/', 'DashboardController@index')->name('painel.index');
 
-    Route::get('/estilos', 'EstiloController@carregarEstilos')->name('painel.estilos.listagem');
+    /*
+     * Grupo de rotas para estilo
+     */
+    Route::group(['prefix' => 'estilos'], function ()
+    {
+        Route::get('/', 'EstiloController@carregarEstilos')->name('painel.estilos.listar');
+        Route::get('/cadastrar', 'EstiloController@cadastrarEstilo')->name('painel.estilos.cadastrar');
+        Route::post('/cadastrar', 'EstiloController@salvarEstilo')->name('painel.estilos.salvar');
+        Route::get('/editar/{id}', 'EstiloController@editarEstilo')->name('painel.estilos.editar');
+        Route::post('/alterar/{id}', 'EstiloController@alterarEstilo')->name('painel.estilos.alterar');
+    });
+
+    /*
+     * Grupo de rotas para albuns
+     */
+    Route::group(['prefix' => 'albuns'], function ()
+    {
+       Route::get('/', 'AlbumController@carregarAlbuns')->name('painel.albuns.listar');
+    });
 });
 
 
